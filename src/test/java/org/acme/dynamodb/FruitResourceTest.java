dfcb47bac5ae02867ff02064d838b1672c185218
Faraz Durrani <farazkhurram@msn.com>
2021-01-30 07:24:23 -0600

    practicing Quarkus connectivity with dymao db

---
+++ src/test/java/org/acme/dynamodb/FruitResourceTest.java
@@ -0,0 +1,17 @@
+package org.acme.dynamodb;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class FruitResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        
+    }
+
+}
